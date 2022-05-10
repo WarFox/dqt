777d8d0af9140a4feadd22e46d57f90d8b660845
Deepu Mohan Puthrote <git@deepumohan.com>
2022-05-10 19:22:25 +0100

    remove unused queries.clj file and related

--- src/dqt/query_runner.clj
+++ src/dqt/query_runner.clj
@@ -2,11 +2,4 @@
   (:require [honey.sql :as honey]
             [honey.sql.helpers :as helpers]
             [next.jdbc :as jdbc]
-            [dqt.queries :as q]
             [next.jdbc.sql :as sql]))
-
-(defn count*
-  [db table]
-  (let [ds (jdbc/get-datasource db)]
-    (sql/query ds
-               (honey/format (q/count* table)))))
