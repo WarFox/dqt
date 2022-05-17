64f83692b418e857ab2534aac6afa95ec431d581
Deepu Mohan Puthrote <git@deepumohan.com>
2022-05-18 00:17:09 +0100

    Refactor run-checks to handle nils and add tests

--- tests/unit/dqt/checks_test.clj
+++ tests/unit/dqt/checks_test.clj
@@ -3,4 +3,11 @@
              [dqt.checks :as sut]))
 
 (deftest run-check-test
-  (is (= [[:metric-column '> 2] true] (sut/run-check [:metric-column '> 2] {:metric-column 20}))))
+  (let [metrics {:metric-column-1 20 :metric-column-2 nil}]
+    (testing "metric-value is not nil"
+      (is (= [[:metric-column-1 '> 2] true]
+             (sut/run-check [:metric-column-1 '> 2] metrics))))
+
+    (testing "metric-value is nil"
+      (is (= [[:metric-column-2 '> 2] nil]
+             (sut/run-check [:metric-column-2 '> 2] metrics))))))
