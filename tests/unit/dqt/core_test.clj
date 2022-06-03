f290cf3d3d8c72420060ef4f4dcac10c12510de0
Deepu Mohan Puthrote <git@deepumohan.com>
2022-06-03 23:02:44 +0100

    Add test: app must call system/init

--- tests/unit/dqt/core_test.clj
+++ tests/unit/dqt/core_test.clj
@@ -1,3 +1,10 @@
 (ns dqt.core-test
   (:require [clojure.test :refer :all]
-            [dqt.core :as sut]))
+            [dqt.core :as sut]
+            [dqt.system :as system]))
+
+(deftest app-test
+  (testing "app initialises system"
+    (with-redefs [system/init (fn [_] :system)]
+      (is (= :system
+             (sut/app {:config :config}))))))
